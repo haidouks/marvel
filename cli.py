a24a99a00c0b730206ec55fd7d520dc245192951
TURKCELL\tccaldanmaz <cansin.aldanmaz@turkcell.com.tr>
2021-09-14 00:00:19 +0300

    flake8 added

--- cli.py
+++ cli.py
@@ -3,19 +3,20 @@ import requests
 import datetime
 from pprint import pprint
 import logging
-#from marvel import Marvel #Did not want to use client :)
+# from marvel import Marvel #Did not want to use client :)
 
-logging.basicConfig( #Set logging format & config
-    level = logging.INFO, #logging.DEBUG to see debug messages
-    format = '%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
-    datefmt = '%Y-%m-%d %H:%M:%S'
-    )
+logging.basicConfig(  # Set logging format & config
+    level=logging.INFO,  # logging.DEBUG to see debug messages
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 timestamp = datetime.datetime.now().strftime('%Y-%m-%d%H:%M:%S')
 
-#Keys received from Marvel
+# Keys received from Marvel
 public_key = 'a66345542c86987744a1702aa4fa6c5a'
 private_key = 'b7b6e3b60bfa422572ef626753fe2c5cb2de8c6b'
+
 
 def hash_params():
     """ Marvel API required md5 hash of timestamp + public key + private key """
@@ -24,34 +25,42 @@ def hash_params():
     hashed_params = hash_md5.hexdigest()
     return hashed_params
 
+
 def search_marvel_characters(keyword: str):
     matching_characters = []
-    #Set request parameters for marvel characters api
+    # Set request parameters for marvel characters api
     params = {
-        'ts': timestamp, 
-        'apikey': public_key, 
+        'ts': timestamp,
+        'apikey': public_key,
         'hash': hash_params(),
         'limit': 50,
-        'offset' : 0 #
-        }
-    do = True 
+        'offset': 0
+    }
+    do = True
     while(do):
 
         logging.debug(msg=f"Checking for offset: {params['offset']}")
         response = requests.get(
-            url = 'https://gateway.marvel.com:443/v1/public/characters',
+            url='https://gateway.marvel.com:443/v1/public/characters',
             params=params
         )
-        results = response.json()["data"]["results"] #Get results from response
+        # Get results from response
+        results = response.json()["data"]["results"]
         for character in results:
-            logging.debug(msg=f'Checking if {character["name"]} matchs {keyword}')
-            if keyword.lower() in character["name"].lower(): # Lower both variable and don't miss matching capital letters
-                logging.debug(msg=f"{character}") # Found character name matching with keyword
+            logging.debug(
+                msg=f'Checking if {character["name"]} matchs {keyword}')
+            # Lower both variable and don't miss matching capital letters
+            if keyword.lower() in character["name"].lower():
+                # Found character name matching with keyword
+                logging.debug(msg=f"{character}")
                 matching_characters.append(character)
-        params["offset"] = params["offset"] + params["limit"] #increase offset on each iteration
-        if params["offset"] > response.json()["data"]["total"]: #Continue while offset is less than total record count 
+        # increase offset on each iteration
+        params["offset"] = params["offset"] + params["limit"]
+        # Continue while offset is less than total record count
+        if params["offset"] > response.json()["data"]["total"]:
             do = False
     return matching_characters
+
 
 if __name__ == "__main__":
     character_name = input("Please enter a keyword: ")
