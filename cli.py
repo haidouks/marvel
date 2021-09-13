6c38bbb01a9195d3acf649b777d6504db9f8a12b
TURKCELL\tccaldanmaz <cansin.aldanmaz@turkcell.com.tr>
2021-09-13 21:27:27 +0300

    Added pytest

--- cli.py
+++ cli.py
@@ -5,7 +5,6 @@ from pprint import pprint
 import logging
 #from marvel import Marvel #Did not want to use client :)
 
-
 logging.basicConfig( #Set logging format & config
     level = logging.INFO, #logging.DEBUG to see debug messages
     format = '%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
@@ -18,8 +17,6 @@ timestamp = datetime.datetime.now().strftime('%Y-%m-%d%H:%M:%S')
 public_key = 'a66345542c86987744a1702aa4fa6c5a'
 private_key = 'b7b6e3b60bfa422572ef626753fe2c5cb2de8c6b'
 
-character_name = input("Please enter a keyword: ")
-
 def hash_params():
     """ Marvel API required md5 hash of timestamp + public key + private key """
     hash_md5 = hashlib.md5()
@@ -27,31 +24,35 @@ def hash_params():
     hashed_params = hash_md5.hexdigest()
     return hashed_params
 
-#Set request parameters for marvel characters api
-params = {
-    'ts': timestamp, 
-    'apikey': public_key, 
-    'hash': hash_params(),
-    'limit': 50,
-    'offset' : 0 #
-    }
+def search_marvel_characters(keyword: str):
+    matching_characters = []
+    #Set request parameters for marvel characters api
+    params = {
+        'ts': timestamp, 
+        'apikey': public_key, 
+        'hash': hash_params(),
+        'limit': 50,
+        'offset' : 0 #
+        }
+    do = True 
+    while(do):
 
-matching_results = 0
-do = True 
-while(do):
-    
-    logging.debug(msg=f"Checking character offset: {params['offset']}")
-    response = requests.get(
-        url = 'https://gateway.marvel.com:443/v1/public/characters',
-        params=params
-    )
-    results = response.json()["data"]["results"] #Get results from response
-    for character in results:
-        logging.debug(msg=f'Checking if {character["name"]} matchs {character_name}')
-        if character_name.lower() in character["name"].lower(): # Lower both variable and don't miss matching capital letters
-            pprint(character) # Found character name matching with keyword
-            matching_results = matching_results + 1
-    params["offset"] = params["offset"] + params["limit"] #increase offset on each iteration
-    if params["offset"] > response.json()["data"]["total"]: #Continue while offset is less than total record count 
-        do = False
-print(f"Found {matching_results} matching results")
+        logging.debug(msg=f"Checking for offset: {params['offset']}")
+        response = requests.get(
+            url = 'https://gateway.marvel.com:443/v1/public/characters',
+            params=params
+        )
+        results = response.json()["data"]["results"] #Get results from response
+        for character in results:
+            logging.debug(msg=f'Checking if {character["name"]} matchs {keyword}')
+            if keyword.lower() in character["name"].lower(): # Lower both variable and don't miss matching capital letters
+                logging.debug(msg=f"{character}") # Found character name matching with keyword
+                matching_characters.append(character)
+        params["offset"] = params["offset"] + params["limit"] #increase offset on each iteration
+        if params["offset"] > response.json()["data"]["total"]: #Continue while offset is less than total record count 
+            do = False
+    return matching_characters
+
+if __name__ == "__main__":
+    character_name = input("Please enter a keyword: ")
+    print(search_marvel_characters(keyword=character_name))
---
+++ cli_test.py
@@ -0,0 +1,35 @@
+import pytest
+import cli
+from random import randint
+
+
+matching_test_keyword = "hulk"
+nonmatching_test_keyword = "qweasdzxc"
+
+@pytest.fixture
+def empty_keyword():
+    '''Returns all characters'''
+    return cli.search_marvel_characters(keyword="")
+
+@pytest.fixture
+def matching_keyword():
+    '''Returns matching characters'''
+    return cli.search_marvel_characters(keyword=matching_test_keyword)
+
+@pytest.fixture
+def nonmatching_keyword():
+    '''Returns empty list'''
+    return cli.search_marvel_characters(keyword=nonmatching_test_keyword)
+
+def test_return_all_characters(empty_keyword):
+    assert len(empty_keyword) >= 1
+    index = randint(0,len(empty_keyword))
+    assert empty_keyword[index].get("name") is not None
+ 
+def test_empty_characters_list(nonmatching_keyword):
+    assert len(nonmatching_keyword) == 0
+
+def test_matching_characters_list(matching_keyword):
+    assert len(matching_keyword) > 0
+    index = randint(0,len(matching_keyword))
+    assert matching_test_keyword.lower() in matching_keyword[index]["name"].lower()
